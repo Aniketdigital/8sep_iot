59298c4f42b581b612fe75600815564db816a1db
Aniketdigital <90948324+Aniketdigital@users.noreply.github.com>
2021-09-24 15:58:56 +0530

    Add files via upload

---
+++ loop_day_3.ino
@@ -0,0 +1,17 @@
+void setup() {
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+
+
+void loop() {
+  for (int i = 12; i >= 9; i--)
+  {
+    digitalWrite(i, 1);
+    delay(500);
+    digitalWrite(i, 0);
+  }
+}
